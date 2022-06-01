a04a81707e4d766195f703f886ad6e96b346091c
Gregoire Chomette <greg.chomette@gmail.com>
2022-06-01 09:59:07 -0700

    dataset.py: Added portion to pre-process population data

--- dataset.py
+++ dataset.py
@@ -77,6 +77,61 @@ class Dataset:
         self.scaler_y = scaler_y
         self.scaler_x = scaler_x
         return
+    
+    def prepare_data_population(self, minLat, maxLat, minLon, maxLon):
+        
+        # Retrieve the data
+        data = pd.read_csv(self.csv_name, usecols=self.inputs+self.outputs)
+        data = data[(data['latitude'] > minLat) & (data['latitude'] < maxLat) & (data['longitude'] > minLon) & (data['longitude'] < maxLon)]
+        
+        # Shuffle the data (if needed)
+        shuffle_data = True
+        if shuffle_data == True:
+            data = data.sample(frac=1)
+            
+        # Create the inputs and outputs
+        data_inputs = data[self.inputs].copy()
+        data_outputs = data[self.outputs].copy()
+
+        # Remove the columns with output = 0
+        data_nozero = data[data[self.outputs[0]] > 0]
+
+        # Normalize input and output with zeros
+        scaler_x = preprocessing.StandardScaler().fit(data_inputs.iloc[:, :].values)
+        self.x = scaler_x.transform(data_inputs.iloc[:, :].values)
+        scaler_y = preprocessing.StandardScaler().fit(data_outputs.iloc[:, :].values)
+        self.y = scaler_y.transform(data_outputs.iloc[:, :].values)
+
+        # Normalize input and output without zeros
+        self.x_nozero = scaler_x.transform(data_nozero.iloc[:, :-1].values)
+        self.y_nozero = scaler_y.transform(data_nozero.iloc[:, -1:].values)
+
+        # Put output in different categories
+        data_outputs[self.outputs[0]].mask(data_outputs[self.outputs[0]] > 0, 1, inplace=True)
+        self.y_class = data_outputs.iloc[:, :].values
+
+        # Verify that we have enough data in our database
+        assert np.shape(self.x)[0] > self.N_test + self.N_val + self.N_train, "The data base contains insufficient data"
+
+        # Split the inputs into test, validation, train, and cross validation sets
+        self.x_test = self.x[0:self.N_test, :]
+        self.x_val = self.x[self.N_test:self.N_test + self.N_val, :]
+        self.x_train = self.x[self.N_test + self.N_val: self.N_test + self.N_val + self.N_train, :]
+        
+        # Split the outputs into test, validation, train, and cross validation sets
+        self.y_test = self.y[0:self.N_test, :]
+        self.y_val = self.y[self.N_test:self.N_test + self.N_val, :]
+        self.y_train = self.y[self.N_test + self.N_val: self.N_test + self.N_val + self.N_train, :]
+
+        # Split the  categorized into test, validation, train, and cross validation sets
+        self.y_class_test = self.y_class[0:self.N_test, :]
+        self.y_class_val = self.y_class[self.N_test:self.N_test + self.N_val, :]
+        self.y_class_train = self.y_class[self.N_test + self.N_val: self.N_test + self.N_val + self.N_train, :]
+
+        # remember scaler 
+        self.scaler_y = scaler_y
+        self.scaler_x = scaler_x
+        return
 
     def save_rescaling_params(self, outputfolder, filename):
 
